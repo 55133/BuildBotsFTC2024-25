0f7f65b66316ba3dc7bfdb10588dfe86f801c508
Lynrea <lynrea@duck.com>
2024-09-22 21:31:19 -0400

    Skeleton Code

---
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
@@ -0,0 +1,16 @@
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Claw {
+
+    Servo snappy;
+private boolean isOpen;
+public Claw(HardwareMap hardwareMap)
+{
+    snappy = hardwareMap.get(Servo.class, "snappy");
+    isOpen = false;
+}
+
+}
