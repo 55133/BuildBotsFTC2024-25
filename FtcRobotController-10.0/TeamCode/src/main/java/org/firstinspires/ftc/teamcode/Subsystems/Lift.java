d8aaebb39c122229be64305c54dd0c31c575808b
Lynrea <katriel@khoffie.com>
2024-10-02 19:16:59 -0400

    updated lift.java code

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Lift.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Lift.java
@@ -15,10 +15,21 @@ public class Lift {
 
     private DcMotor slider;
 
+    // "0" is a placeholder--don't get confused Katriel!
+    private double Kp = 0;
+    private double Ki = 0;
+    private double Kd = 0;
+
+    private int reference = 0;
+    private int integralSum = 0;
+    private int lastError = 0;
+
+    private static ElapsedTime timer = new ElapsedTime();
+
 
     Telemetry telemetry;
 
-    public Lift(HardwareMap hardwareMap, Telemetry tele)
+        public Lift(HardwareMap hardwareMap, Telemetry tele)
     {
         telemetry = tele;
 
@@ -33,12 +44,29 @@ public class Lift {
 
     }
 
-    public void runToPosition(int pos){
+        public void runToPosition(int pos){
 
-        
+        timer.reset();
+        //slider.setTargetPosition(pos);
+        //slider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        reference = pos
+        encoderPosition = slider.getPosition();
+        error = reference - encoderPosition;
+
+        derivative = (error - lastError) / timer.seconds();
+        integralSum = integralSum + (error * timer.seconds());
+
+        out = (Kp * error) + (Ki * integralSum) + (Kd * derivative);
+
+        setPower(out);
+
+        lastError = error;
+
+        timer.reset();
     }
 
-    public void setPower(double pfl)
+        public void setPower(double pfl)
     {
         slider.setPower(pfl);
 
